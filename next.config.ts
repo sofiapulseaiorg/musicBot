be953654fe73201d5b5190943892c615b0131426
dmncfrtn-sp <dom@sofiapulse.com>
2025-04-17 12:56:09 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // no need to set runtime here
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
